14efe7fc3708939de3a81374029f9a778908ca7c
akkupallipooja@gmail.com <akkupallipooja@gmail.com>
2026-01-23 19:32:23 +0530

    Updated java script.js with new code

--- java script.js
+++ java script.js
@@ -171,4 +171,139 @@
 // console.log(name);
 // console.log(dept);
 // console.log(marks);
+// for..in loop
+// let arr=[10,20,30,40];
+// for(let i in arr)
+// {
+//     console.log(arr[i]);
+// }
+// for object we get keys only.
+// var obj={
+//     name:"A.pooja",
+//     age:20
+// }
+// for (let i in obj)
+// {
+//     console.log(i,obj[i]);
+// }
+// for(variable variable_name of array_name)
+// {
+//     statement
 
+// }
+// for (let val of arr)
+// {
+//     console.log(val);
+// }
+// map used to change the values
+let arr=[1,2,3,4];
+// var double_arr=arr.map((i)=>(i*2));
+// console.log(double_arr)
+// filter
+// var even=arr.filter((i)=>(i%2===0))
+// console.log(even) 
+// reduce
+// var total=arr.reduce((sum,i)=>(sum+i),0);
+// console.log(total);
+// combine all(map,filter,reduce)
+// var double_arr1=arr.map((i)=>(i*2));
+// var even1=double_arr1.filter((i)=>(i%2===0));
+// var total1=even1.reduce((sum,i)=>(sum+i),0);
+// console.log(double_arr1);
+// console.log(even1);
+// console.log(total1);
+// var total_sum=arr.map((num)=>num*2).filter((num)=>(num%2===0)).reduce((sum,num)=>(sum+num));
+// console.log(total_sum)
+// prime numbers in a range
+// for (let i=2;i<=10;i++){
+//     let isprime=true;
+//     for(let j=2;j<=i-1;j++)
+//     {
+//         if (i%j===0)
+//         {
+//             isprime=false;
+//             break;
+//         }
+//     }
+//     if(isprime)
+//     {
+//        console.log(i); 
+//     }
+
+// }
+// to check given number is prime or not.
+// x=23;
+// isprime=true;
+// for (let i=2;i<=Math.sqrt(x);i++)
+// {
+// if (x%i===0)
+// {
+// isprime=false;
+// break;
+// }
+// }
+// if(isprime)
+// {
+//     console.log("It is a prime number");
+// }
+// var x=8;
+// var flag=true;
+// if(n<=1)flag 
+// var name="A.pooja";
+// console.log(`My name is ${name}`);
+// arr.forEach((value,index)=>
+// {
+//     console.log(index,value);
+// })
+// var add=(a,b,callback)=>{
+//     var result=a+b;
+//     callback(result);
+
+// }
+// add(10,20,(result)=>
+// {
+//     console.log(result);
+// })
+// var demo=()=>
+// {
+//     console.log("I am callback");
+// }
+// var main=(myFunction)=>
+// {
+//     myFunction()
+// }
+// main(demo);
+// var promise = new Promise((resolve,reject)=>
+// {
+//     var success =true;
+//     if(success)
+//     {
+//         setTimeout(()=>{
+//         resolve("Promise Resolved");
+
+//         },3000)
+//     }
+//     else{
+//         reject("promise is rejected");
+//     }
+// })
+// promise.then((res)=>console.log(res)).catch((err)=>console.log(err))
+// const getData=()=>
+// {
+//     return fetch("https://jsonplaceholder.typicode.com/posts")
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch(err=>console.log(err))
+// const getData=async()=>{
+//     try{
+//     var res= await fetch("https://jsonplaceholder.typicode.com/posts")
+//     var data= await res.json();
+//     console.log(data);
+//     }
+//     catch (err)
+//     {
+//         console.log(err);
+//     }
+// }
+// getData();
