14f996619a8223277df41ed05d390e7ff7d2981a
akkupallipooja@gmail.com <akkupallipooja@gmail.com>
2026-01-27 19:16:01 +0530

    project updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,11 @@ import Counter from './component/Counter'
 import  {Route,Routes} from 'react-router-dom'
 import UseState from './Hooks/UseState'
 import Login from './auth/Login'
+import Hook from'./Hooks/Hook'
+import UseEffect from './Hooks/UseEffect'
+import UseEffectApi from './Hooks/UseEffectApi'
+import Signedup from './auth/Signedup'
+import UseRef from './Hooks/UseRef'
 const element=<h1>Hello</h1>
 const App = () => {
   return (
@@ -19,8 +24,15 @@ const App = () => {
     <Route path='/about' element={<About name="A.pooja" age={25} dept="Cyber Security"/>}/>
     <Route path='/service' element={<Service/>}/>
     <Route path='/contact'element={<Contact/>}/>
-    <Route path='/useState'element={<UseState/>}/>
     <Route path='/login' element={<Login/>}/>
+    <Route path='/signedup' element={<Signedup/>}/>
+
+    <Route path='/hook' element={<Hook/>}>
+    <Route path='useState'element={<UseState/>}/>
+    <Route path='useEffect' element={<UseEffect/>}/>
+    <Route path='useEffectApi' element={<UseEffectApi/>}/>
+    <Route path='useRef' element={<UseRef/>}/>
+    </Route>
    </Routes>
    </>
   )
--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -11,8 +11,10 @@ const Navbar = () => {
           <li><Link className='link' to="/about">About</Link></li>
           <li><Link className='link'to="/contact">Contact</Link></li>
           <li><Link className='link'to="/service">Services</Link></li>
-          <li><Link className='link'to="/useState">UseState</Link></li>
+          <li><Link className='link'to="/hook">Hook</Link></li>
           <li><Link className='link'to="/login">Login</Link></li>
+          <li><Link className='link'to="/useState">UseState</Link></li>
+          <li><Link className='link'to="/signedup">Signedup</Link></li>
         </ul>
       </nav>
     </div>
