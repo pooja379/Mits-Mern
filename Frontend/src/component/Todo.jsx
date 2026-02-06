7dd0a26835a048077078ccb85c188d89fd2d3529
akkupallipooja@gmail.com <akkupallipooja@gmail.com>
2026-02-06 12:09:31 +0530

    Backend Hosted and api connected with todo

--- Frontend/src/component/Todo.jsx
+++ Frontend/src/component/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = () => {
     const[task,setTask]=useState("");
     const[todos,setTodos]=useState([]);
     const[editing,setEditing]=useState(null);
-    const API="http://localhost:3000";
+    const API="https://mits-mern.onrender.com";
     const fetchData=async(req,res)=>{
         const response=await axios.get(`${API}/api/todo/`)
         setTodos(response.data);
